8eb317d237d510abe3b3eed580993973dd7a551c
0ceanlight <imahacubak@gmail.com>
2025-06-26 21:09:10 +0200

    Update automation script to skip confirmation

--- automation/collect_data.sh
+++ automation/collect_data.sh
@@ -2,5 +2,5 @@
 
 for config_file in config/sim_config*.ini; do
   echo "Running simulation with config: $config_file"
-  python3 main_loop.py --sim-config "$config_file"
+  python3 main_loop.py --sim-config "$config_file" --noconfirm
 done
--- scripts/collect_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for config_file in config/sim_config*.ini; do
-  echo "Running simulation with config: $config_file"
-  python3 main_loop.py --sim-config "$config_file"
-done
